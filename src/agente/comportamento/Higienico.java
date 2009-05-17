8e1ea688a46c4e65222a6f3be1dba731e9872208
heliokann <heliokann@553c1e12-4299-11de-9a54-b9394989f659>
2009-05-17 20:12:36 +0000

    criacao da estrutura inicial

---
+++ src/agente/comportamento/Higienico.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package agente.comportamento;
+
+/**
+ *
+ * @author heliokann
+ */
+public class Higienico extends Comportamento{
+    
+    public Higienico(){
+        
+    }
+    
+    @Override
+    protected void carregaCrencas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+
+}
