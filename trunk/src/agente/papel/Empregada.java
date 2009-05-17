001eedad2a5259b4571e3aec66341118651ee7d7
heliokann <heliokann@553c1e12-4299-11de-9a54-b9394989f659>
2009-05-17 04:54:55 +0000

    criacao da estrutura inicial

---
+++ trunk/src/agente/papel/Empregada.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package agente.papel;
+
+import framework.agentRole.AgentRole;
+
+/**
+ *
+ * @author heliokann
+ */
+public class Empregada extends AgentRole{
+    
+    boolean manterResidenciaHabitavel;
+    
+    public Empregada(){
+        
+    }
+
+}
