553917303e5ec0c22f8cbfcdd24052adc8f7a3ec
heliokann <heliokann@553c1e12-4299-11de-9a54-b9394989f659>
2009-05-17 04:54:55 +0000

    criacao da estrutura inicial

---
+++ src/acao/AcaoLimpar.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package acao;
+
+import framework.agent.Agent;
+import framework.mentalState.Action;
+import framework.organization.MainOrganization;
+
+/**
+ *
+ * @author heliokann
+ */
+public class AcaoLimpar extends Action{
+    
+    @Override
+    public boolean execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void execute(MainOrganization organizacaoPrincipal, Agent agente) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
