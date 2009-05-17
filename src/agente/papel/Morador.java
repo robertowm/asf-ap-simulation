d4c7cba288d891952b03c44dd00bbfa0d16988ca
heliokann <heliokann@553c1e12-4299-11de-9a54-b9394989f659>
2009-05-17 04:33:10 +0000

    primeiro commit da versao

---
+++ src/agente/papel/Morador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package agente.papel;
+
+import framework.agentRole.AgentRole;
+
+/**
+ *
+ * @author heliokann
+ */
+public class Morador extends AgentRole{
+    
+    boolean manterResidenciaHabitavel;
+    
+    public Morador(){
+        
+    }
+
+}
---
+++ src/organizacao/Habitacao.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package organizacao;
+
+import framework.FIPA.ElementID;
+import framework.FIPA.communication.MTP;
+import framework.environment.MTS_Environment;
+import framework.mentalState.Plan;
+import framework.mentalState.goal.Goal;
+import framework.organization.MainOrganization;
+import java.util.Collection;
+
+/**
+ *
+ * @author heliokann
+ */
+public class Habitacao extends MainOrganization{
+    
+    public Habitacao(MTS_Environment ambiente, ElementID idElemento, MTP  protocoloTransporteMensagem){
+        super(ambiente, idElemento, protocoloTransporteMensagem);
+    }
+
+    @Override
+    protected Plan selectingPlan(Collection arg0, Goal arg1) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void executingPlan(Plan arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected Goal selectingGoalToAchieve() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected boolean checkIfWillContinue() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+
+}
