b3ac32603fbb0d20125b48921da14bf5bbdb7562
robertowm <robertowm@553c1e12-4299-11de-9a54-b9394989f659>
2009-05-18 00:21:40 +0000

    Criação de uma classe para armazenar as constantes da aplicação

---
+++ src/util/ConstantesAplicacao.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package util;
+
+/**
+ *
+ * @author robertowm
+ */
+public interface ConstantesAplicacao {
+
+    // >>> Plataforma <<<
+    public static final String PLATAFORMA_NOME = "PlataformaResidencia";
+
+
+    // >>> Ambientes <<<
+    // >>>>>> APJAVA
+    public static final String AMBIENTE_APJAVA_NOME = "Residencia_APJVA";
+
+
+    // >>> MessageTransportProtocol <<<
+    public static final String MTP_PORTA = "1500";
+
+    // >> Outros <<
+    public static final String LOCAL_HOST = "127.0.0.1";
+
+
+}
