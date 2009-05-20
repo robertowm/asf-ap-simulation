1f1b2c54a6772ea10ccf9e3bdb631dfa79200854
heliokann <heliokann@553c1e12-4299-11de-9a54-b9394989f659>
2009-05-20 00:58:21 +0000

    biblioteca com codigo

--- trunk/src/agente/comportamento/Higienico.java
+++ trunk/src/agente/comportamento/Higienico.java
@@ -5,6 +5,8 @@
 
 package agente.comportamento;
 
+import framework.mentalState.belief.LeafBelief;
+
 /**
  *
  * @author heliokann
@@ -17,7 +19,11 @@ public class Higienico extends Comportamento{
     
     @Override
     protected void carregaCrencas() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        crencas.add(new LeafBelief("int", "dessarruma", 15));
+        crencas.add(new LeafBelief("int", "arruma", 15));
+        crencas.add(new LeafBelief("int", "limpa", 60));
+        crencas.add(new LeafBelief("int", "suja", 10));
+        crencas.add(new LeafBelief("int", "chamaEmpregada", true));
     }
     
     
